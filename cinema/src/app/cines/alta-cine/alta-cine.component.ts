15bff0be9386bfa2270803ef06bc1470b8f29dd2
LETICIA ORIVE PEÑA <leticia.orive.contractor@bbva.com>
2023-06-06 11:36:19 +0200

    Código TypeScript completo

--- cinema/src/app/cines/alta-cine/alta-cine.component.ts
+++ cinema/src/app/cines/alta-cine/alta-cine.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Cine, Sala } from './../cines.model';
+import { CineService } from './../cines.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-alta-cine',
   templateUrl: './alta-cine.component.html',
-  styleUrls: ['./alta-cine.component.css']
+  styleUrls: ['./alta-cine.component.css'],
 })
-export class AltaCineComponent {
+export class AltaCineComponent implements OnInit {
+  cine!: Cine;
+  sala!: Sala[];
 
+  constructor(private cineService: CineService) {}
+
+  ngOnInit() {
+    this.cine = this.cineService.nuevoCine();
+    this.sala = this.cineService.getSala();
+  }
+
+  nuevoCine(): void {
+    this.cineService.agregarCine(this.cine);
+    this.cine = this.cineService.nuevoCine();
+  }
 }
--- cinema/src/app/peliculas/alta-pelicula/alta-pelicula.component.ts
+++ cinema/src/app/peliculas/alta-pelicula/alta-pelicula.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Pelicula } from '../peliculas.model';
+import { PeliculasService } from '../peliculas.service';
 @Component({
   selector: 'app-alta-pelicula',
   templateUrl: './alta-pelicula.component.html',
-  styleUrls: ['./alta-pelicula.component.css']
+  styleUrls: ['./alta-pelicula.component.css'],
 })
-export class AltaPeliculaComponent {
+export class AltaPeliculaComponent implements OnInit {
+  pelicula!: Pelicula;
 
+  constructor(private peliculaService: PeliculasService) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  nuevaPelicula(): void {
+    this.peliculaService.agregarPelicula(this.pelicula);
+    this.pelicula = this.peliculaService.nuevaPelicula();
+  }
 }
