79822cc9dd6c9d6d08709395efdcd24511f68000
LETICIA ORIVE PEÑA <leticia.orive.contractor@bbva.com>
2023-06-06 12:14:38 +0200

    componentes de listados

--- cinema/src/app/cines/cines.module.ts
+++ cinema/src/app/cines/cines.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from '@angular/common';
 import { CineService } from './cines.service';
 import { AltaCineComponent } from './alta-cine/alta-cine.component';
 import { FormsModule } from '@angular/forms';
+import { ListadoCinesComponent } from './listado-cines/listado-cines.component';
 @NgModule({
-  declarations: [AltaCineComponent],
+  declarations: [AltaCineComponent, ListadoCinesComponent],
   imports: [CommonModule, FormsModule],
   providers: [CineService],
   exports: [AltaCineComponent],
--- cinema/src/app/peliculas/peliculas.module.ts
+++ cinema/src/app/peliculas/peliculas.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from '@angular/common';
 import { PeliculasService } from './peliculas.service';
 import { AltaPeliculaComponent } from './alta-pelicula/alta-pelicula.component';
 import { FormsModule } from '@angular/forms';
+import { ListadoPeliculasComponent } from './listado-peliculas/listado-peliculas.component';
 @NgModule({
-  declarations: [AltaPeliculaComponent],
+  declarations: [AltaPeliculaComponent, ListadoPeliculasComponent],
   imports: [CommonModule, FormsModule],
   providers: [PeliculasService],
   exports: [AltaPeliculaComponent],
